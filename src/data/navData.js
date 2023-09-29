38bc1b9b94116070dc69626483c15c1f678b3edc
Ravi Rambaran <raviramba@gmail.com>
2023-09-30 01:16:16 +0200

    some js link behaviour update

---
+++ src/data/navData.js
@@ -0,0 +1,63 @@
+const navData = [
+    {
+        name: "Gallery",
+        path: "/",
+        shown: true,
+        location: "internal",
+    },
+    {
+        name: "About",
+        path: "/about/",
+        shown: true,
+        location: "internal",
+    },
+    {
+        name: "Contact",
+        path: "/contact/",
+        shown: true,
+        location: "internal",
+    },
+    {
+        name: "Blog",
+        path: "/blog/",
+        shown: false,
+        location: "internal",
+    },
+    {
+        name: "Patreon",
+        href: "https://www.patreon.com/spookeydonkey",
+        image: "/src/picca/gfx/patreon.png",
+        shown: true,
+        location: "external",
+    },
+    {
+        name: "Facebook",
+        href: "https://www.facebook.com/rlmkr/",
+        image: "/src/picca/gfx/fb.png",
+        shown: true,
+        location: "external",
+    },
+    {
+        name: "Tumblr",
+        href: "https://ravirambaran.tumblr.com/",
+        image: "/src/picca/gfx/tumblr.png",
+        shown: true,
+        location: "external",
+    },
+    {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/ravirambaran/",
+        image: "/src/picca/gfx/linkedin.png",
+        shown: true,
+        location: "external",
+    },
+    {
+        name: "Instagram",
+        href: "https://instagram.com/ravirambaranart/",
+        image: "/src/picca/gfx/insta.png",
+        shown: true,
+        location: "external",
+    },
+];
+
+export default navData;
