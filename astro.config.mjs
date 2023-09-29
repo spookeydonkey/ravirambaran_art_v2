bdc25af4b094ccf0e6661c5ca42c87c06176aa61
Ravi Rambaran <raviramba@gmail.com>
2023-09-29 22:31:19 +0200

    start project v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // alpine
+  integrations: [alpine(), mdx()]
+  // alpine
+});
---
+++ src/data/socialMediaPlatform.ts
@@ -0,0 +1,28 @@
+export const socialMediaPlatforms = [
+    {
+      href: "https://www.patreon.com/spookeydonkey",
+      altText: "Ravi's Patreon",
+      imagePath: "/src/picca/gfx/patreon.png",
+    },
+    {
+      href: "https://www.facebook.com/rlmkr/",
+      altText: "Ravi's Facebook",
+      imagePath: "/src/picca/gfx/fb.png",
+    },
+    {
+      href: "https://ravirambaran.tumblr.com/",
+      altText: "Ravi's Tumblr",
+      imagePath: "/src/picca/gfx/tumblr.png",
+    },
+    {
+      href: "https://instagram.com/ravirambaranart/",
+      altText: "Ravi's Instagram",
+      imagePath: "/src/picca/gfx/insta.png",
+    },
+    {
+      href: "https://www.linkedin.com/in/ravirambaran/",
+      altText: "Ravi's LinkedIn",
+      imagePath: "/src/picca/gfx/linkedin.png",
+    },
+    // add more social media platforms here
+  ];
